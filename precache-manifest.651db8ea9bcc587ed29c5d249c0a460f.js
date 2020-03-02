91b3be692823ce8bb4a25d1064bf4edd1bef23ba
jackklop <jackklop123@gmail.com>
2020-03-02 18:39:09 -0500

    Updates

---
+++ precache-manifest.651db8ea9bcc587ed29c5d249c0a460f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff383b54b9ef1944125730d7a483dce",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "a2770ca8d99c9edb30f5",
+    "url": "/food-recipe/static/css/main.4310bdba.chunk.css"
+  },
+  {
+    "revision": "5d4c5feced20b14ef64d",
+    "url": "/food-recipe/static/js/2.097d202e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/food-recipe/static/js/2.097d202e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2770ca8d99c9edb30f5",
+    "url": "/food-recipe/static/js/main.0b1f3996.chunk.js"
+  },
+  {
+    "revision": "8321fd37765e32f7dca8",
+    "url": "/food-recipe/static/js/runtime-main.5ec8a7cd.js"
+  }
+]);
